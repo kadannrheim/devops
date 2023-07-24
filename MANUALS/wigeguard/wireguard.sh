29545f51535d171a3deabd2e689eda798d7b44be
Evgeniy Alifanov <kadannr@gmail.com>
2023-07-24 17:43:03 +0300

    added wireguard config

---
+++ MANUALS/wigeguard/wireguard.sh
@@ -0,0 +1,7 @@
+# https://blog.komow.ru/wg
+
+# apt update
+apt upgrade
+# apt dist-upgrade
+apt install wireguard iptables python3-pip git mc htop -y
+git clone -b v3.0.6 https://github.com/donaldzou/WGDashboard.git wgdashboard
