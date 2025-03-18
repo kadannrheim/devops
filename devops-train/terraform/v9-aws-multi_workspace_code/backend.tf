f52491781626e55fdcf96313645b9319298a5583
kadannr <kadannr@gmail.com>
2025-03-18 09:07:37 +0300

    task15 problem with backet

--- devops-train/terraform/v9-aws-multi_workspace_code/backend.tf
+++ devops-train/terraform/v9-aws-multi_workspace_code/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket                      = "devopstrain-bucket-kadannr-6" //имя созданного backet из задания 7
+    bucket                      = "devopstrain-bucket-kadannr" //имя созданного backet из задания 7
     key                         = "terraform/aws-vpc/state"
     region                      = "eu-west-2"
   }
